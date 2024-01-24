7333f12944c7a461f6754e28df089054f52af6a1
pankaj <pankajbehera752@gmail.com>
2024-01-24 14:25:51 +0530

    Initial commit

---
+++ src/components/dashboard/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from "../../common/sidebar";
+
+const Dashboard = () => {
+  return (
+    <>
+        <Sidebar/>
+        <h1>pankaj</h1>
+    </>
+  )
+}
+
+export default Dashboard
