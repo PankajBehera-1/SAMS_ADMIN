c560b14f3e7df51b505fdfb5938993f22b6ebb14
lulu <daslaxmikanta219@gmail.com>
2024-02-02 09:57:17 +0530

    notice board lulu

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "../components/dashboard/index";
+import Noticeboard from "../components/noticeboard/index";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />}/>
+        <Route path="/noticeboard" element={<Noticeboard />}/>
+
       </Routes>
     </Router>
   );
