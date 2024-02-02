094e76ebc476db027b3db9dcbcd178a7ef5a1a87
AnuragNR21 <anuragnayak2111@gmail.com>
2024-02-02 18:10:23 +0530

    merger issue resolved

--- src/routers/router.js
+++ src/routers/router.js
@@ -5,7 +5,7 @@ import Dashboard from "../components/dashboard/index";
 import Noticeboard from "../components/noticeboard/index";
 import AddProfessors from "../components/professors/addProfessors";
 import ViewProfessors from "../components/professors/viewProfessors";
-import Notice from "../components/notice/index";
+// import Notice from "../components/notice/index";
 import StudentDetails from "../components/registration/studentDetails";
 import Gallery from "../components/gallery/index";
 
@@ -21,7 +21,7 @@ const AppRouter = () => {
         <Route path="/admission" element={<Admission />} />
         <Route path="/add_professors" element={<AddProfessors />} />
         <Route path="/view_professors" element={<ViewProfessors />} />
-        <Route path="/notice" element={<Notice />} />
+        <Route path="/noticeboard" element={<Noticeboard />} />
         <Route path="/student" element={<StudentDetails />} />
         <Route path="/gallery" element={<Gallery />} />
         
